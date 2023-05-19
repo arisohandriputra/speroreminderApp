ad22119c204b46737bf3e077947db00df13c8e05
Ari Sohandri Putra <86937341+arisohandriputra@users.noreply.github.com>
2023-05-19 16:13:41 +0800

    Modify Time Based Location

---
+++ Spero_Base.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading;
+using CommandLine;
+using SharpAudio.Codec;
+
+namespace SharpAudio.Sample
+{
+    class Program
+    {
+        public class Options
+        {
+            [Option('i', "input", Required = true, HelpText = "Specify the file(s) that should be played")]
+            public IEnumerable<string> InputFiles { get; set; }
+
+            [Option('v', "volume", Required = false, HelpText = "Set the output volume (0-100).", Default = 100)]
+            public int Volume { get; set; }
+        }
+
+        static void Main(string[] args)
+        {
+            CommandLine.Parser.Default.ParseArguments<Options>(args)
+              .WithParsed<Options>(opts => RunOptionsAndReturnExitCode(opts));
+        }
+
+        private static void RunOptionsAndReturnExitCode(Options opts)
+        {
+            using (var engine = AudioEngine.CreateDefault())
+            {
+
+                if (engine == null)
+                {
+                    Console.WriteLine("Failed to create an audio backend!");
+                }
+
+                foreach (var file in opts.InputFiles)
+                {
+                    var soundStream = new SoundStream(File.OpenRead(file), engine);
+
+                    soundStream.Volume = opts.Volume / 100.0f;
+
+                    soundStream.Play();
+
+                    Console.WriteLine("Playing file with duration: " + soundStream.Duration);
+
+                    while (soundStream.IsPlaying)
+                    {
+                        Thread.Sleep(100);
+                    }
+                }
+            }
+        }
+    }
+}
